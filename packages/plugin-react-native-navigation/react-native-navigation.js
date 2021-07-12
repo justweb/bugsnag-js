5addf64e27ff6ad6eb196d6695fd7989a38480b0
Joe Haines <hello@joehaines.co.uk>
2021-07-12 17:29:21 +0100

    Debug RN Navigation plugin

--- packages/plugin-react-native-navigation/react-native-navigation.js
+++ packages/plugin-react-native-navigation/react-native-navigation.js
@@ -10,12 +10,27 @@ module.exports = class BugsnagPluginReactNativeNavigation {
   }
 
   load (client) {
+    client.addMetadata('debugging (on load)', {
+      time: (new Date()).toString(),
+      enabledBreadcrumbTypes: client._config.enabledBreadcrumbTypes,
+    })
+
     let lastComponent
 
     this.Navigation.events().registerComponentDidAppearListener(event => {
+      client.addMetadata('debugging (registerComponentDidAppearListener)', {
+        time: (new Date()).toString(),
+        lastComponent,
+        currentComponent: event.componentName,
+        enabledBreadcrumbTypes: client._config.enabledBreadcrumbTypes,
+        isBreadcrumbTypeEnabled: client._isBreadcrumbTypeEnabled('navigation')
+      })
+
       client.setContext(event.componentName)
 
       if (lastComponent !== event.componentName && client._isBreadcrumbTypeEnabled('navigation')) {
+        client.addMetadata('debugging (registerComponentDidAppearListener)', 'leftBreadcrumb', true)
+
         client.leaveBreadcrumb(
           'React Native Navigation componentDidAppear',
           { to: event.componentName, from: lastComponent },
